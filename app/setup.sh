5741f5fba1fd0cf4892f80776e6928e5e59e7ff0
Ryan Liang <jiallian@amazon.com>
2024-08-17 02:06:27 -0400

    Create a setup script
    Signed-off-by: Ryan Liang <jiallian@amazon.com>

---
+++ app/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Starting setup..."
+
+# Install npm dependencies
+echo "Installing dependencies..."
+npm install
+
+# Run any additional setup commands here
+
+echo "Setup complete! You can now run the project using 'npm start'."
